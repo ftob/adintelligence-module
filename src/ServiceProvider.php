f0242a1cf109dd65cf1067088d3b10741136f77e
Nikita Volkov <litebackend@gmail.com>
2017-10-10 01:35:45 +0700

    Extend laravel service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 namespace AdIntelligence\Client;
 
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
-class ServiceProvider {
+class ServiceProvider extends LaravelServiceProvider
+{
     //
 }
