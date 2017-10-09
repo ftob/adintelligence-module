4c3f0912f5362060352a214b4c6fc544b6839efa
Nikita Volkov <litebackend@gmail.com>
2017-10-10 01:47:44 +0700

    Merge config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,7 +3,22 @@ namespace AdIntelligence\Client;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
+/**
+ * Class ServiceProvider
+ * @package AdIntelligence\Client
+ */
 class ServiceProvider extends LaravelServiceProvider
 {
-    //
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/Resources/config/parameter.php', 'adintelelligence'
+        );
+    }
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/Resources/config/parameter.php' => config_path('adintelelligence.php'),
+        ]);
+    }
 }
